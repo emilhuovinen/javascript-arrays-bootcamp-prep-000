141d742d3ea6ace0405cde357569027143e62a04
emil.huo@gmail.com <emil.huo@gmail.com>
2018-09-12 19:16:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,11 @@ var chocolateBars = [
   "kitkat",
   "skittles"
   ];
+  
+ var addElementToBeginningOfArray = function(array, element) {
+   
+ }
+ 
+ var destructivelyAddElementToBeginningOfArray = function(array, element) {
+   
+ }
