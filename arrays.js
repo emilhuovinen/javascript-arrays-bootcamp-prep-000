6df80a76bcfb7c1578cb11f0dfa695066526c9f9
emil.huo@gmail.com <emil.huo@gmail.com>
2018-09-12 19:28:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ var chocolateBars = [
  }
  
  var destructivelyAddElementToBeginningOfArray = function(array, element) {
-   return array.unshift(element)
+   return array.unshift()
  }
  
 var addElementToEndOfArray = function(array, element) {
